b4eedf7edf7d1d95ae5b4a77de357efce3d3807f
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2023-03-05 12:57:42 +0100

    Update BookingPage.jsx

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -6,10 +6,17 @@ import {
     Toolbar,
     Typography,
     Button,
+    Stepper,
+    Step,
+    StepLabel,
+    StepContent,
 } from '@mui/material';
 import { DateCalendar } from '@mui/x-date-pickers';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function BookingPage() {
+    const navigate = useNavigate();
+    const [activeStep, setActiveStep] = useState(0);
 
     return <Page
         header={
@@ -25,7 +32,79 @@ function BookingPage() {
     >
         <Typography variant='h5'>Időpont foglalás</Typography>
 
-        <DateCalendar/>
+        <Stepper activeStep={activeStep} orientation='vertical'>
+            <Step>
+                <StepLabel>Foglalkozás</StepLabel>
+                <StepContent>
+                    <Typography>Választott foglalkozás:</Typography>
+                    <Button
+                        variant="contained"
+                        onClick={() => { navigate('/'); }}
+                    >
+                        Vissza
+                    </Button>
+                    <Button
+                        variant="contained"
+                        onClick={() => { setActiveStep(activeStep + 1); }}
+                    >
+                        Tovább
+                    </Button>
+                </StepContent>
+            </Step>
+            <Step>
+                <StepLabel>Nap</StepLabel>
+                <StepContent>
+                    <DateCalendar />
+                    <Button
+                        variant="contained"
+                        onClick={() => { setActiveStep(activeStep - 1); }}
+                    >
+                        Vissza
+                    </Button>
+                    <Button
+                        variant="contained"
+                        onClick={() => { setActiveStep(activeStep + 1); }}
+                    >
+                        Tovább
+                    </Button>
+                </StepContent>
+            </Step>
+            <Step>
+                <StepLabel>Idő</StepLabel>
+                <StepContent>
+                    <Typography>Szabad időpontok helye itt.</Typography>
+                    <Button
+                        variant="contained"
+                        onClick={() => { setActiveStep(activeStep - 1); }}
+                    >
+                        Vissza
+                    </Button>
+                    <Button
+                        variant="contained"
+                        onClick={() => { setActiveStep(activeStep + 1); }}
+                    >
+                        Tovább
+                    </Button>
+                </StepContent>
+            </Step>
+            <Step>
+                <StepLabel>Foglalás</StepLabel>
+                <StepContent>
+                <Button
+                        variant="contained"
+                        onClick={() => { setActiveStep(activeStep - 1); }}
+                    >
+                        Vissza
+                    </Button>
+                    <Button
+                        variant="contained"
+                        onClick={() => { /* TODO */ }}
+                    >
+                        Foglalás
+                    </Button>
+                </StepContent>
+            </Step>
+        </Stepper>
 
     </Page>;
 }
